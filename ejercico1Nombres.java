b9481337d24c3950e75da0ed58dc6edc28549322
David Castro <dcassor814@iescarrillo.es>
2024-03-04 13:58:21 +0100

    inicio proyecto

---
+++ ejercico1Nombres.java
@@ -0,0 +1,26 @@
+package ejercicio1;
+
+import java.util.ArrayList;
+
+public class ejercico1Nombres {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> nombres = new ArrayList<String>();
+		
+		nombres.add("Juan");
+		nombres.add("Pedro");
+		nombres.add("Luis");
+		nombres.add("Carlos");
+		nombres.add("Ana");
+		
+		
+		int contador = 1;
+		for (String nombre : nombres) {
+			System.out.println("Nombre " + contador + ": " + nombre);
+			contador++;
+		}
+
+	}
+
+}
